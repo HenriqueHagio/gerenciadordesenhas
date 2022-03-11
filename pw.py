3ac64a04d379034f965998d429626cd9e38c0b7e
HenriqueHagio <101066434+HenriqueHagio@users.noreply.github.com>
2022-03-11 15:37:44 -0500

    Create pw.py
    um pequeno gerenciador de senhas

---
+++ pw.py
@@ -0,0 +1,20 @@
+#! python3
+#pw.py - Um programa que armazena suas senhas NÃO é seguro, apenas para fins de estudo
+
+PASSWORDS = {'email':'Mjfo#rIbv4N&',
+	'blog':'W@E5DRKHoLuE',
+	'senhadolol':'lPdR!SF!%Cpj',
+	'Facebook':'U^PcW&SrzTKv'}
+
+import sys, pyperclip
+if len(sys.argv)<2:
+   print('Usage: python pw.py[account]-copy account password')
+   sys.exit()
+
+account=sys.argv[1] #o primeiro argumente da linha de comando é o nome da conta
+
+if account in PASSWORDS:
+   pyperclip.copy(PASSWORDS[account])
+   print('Password for ' + account + ' copied to clipboard.')
+else:
+   print('There is no account named' + account)	
